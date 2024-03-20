91dd362372d4b0118475351c5aefa486ff19bef3
Octavio Riccardo <octariccardo@gmail.com>
2024-03-19 23:36:27 -0300

    Update App.tsx

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,12 +1,37 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-
+import * as React from 'react';
 import './App.css';
-import { ChakraProvider } from '@chakra-ui/react';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  ChakraProvider,
+  Box,
+} from '@chakra-ui/react';
 
 function Hello() {
   return (
     <ChakraProvider>
-
+      <Accordion>
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box as="span" flex="1" textAlign="left">
+                Section 1 title
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
     </ChakraProvider>
   );
 }
