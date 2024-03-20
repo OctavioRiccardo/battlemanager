939b662e3bcfd0e8636f0d4c6308fc76da627db4
Octavio Riccardo <octariccardo@gmail.com>
2024-03-19 22:32:03 -0300

    .

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,7 +1,7 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+
 import './App.css';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
 
 function Hello() {
   return (
